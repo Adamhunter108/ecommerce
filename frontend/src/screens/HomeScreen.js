2bcc5585ecb3e2fd1a2acb827a9d09f3b64a89b1
Adamhunter108 <61752774+Adamhunter108@users.noreply.github.com>
2021-08-27 16:43:13 -0400

    Created productActions and moved API call from HomeScreen and cleaned up HomeScreen

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,7 +1,11 @@
 import React, { useState, useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import { Row, Col } from 'react-bootstrap'
 import Product from '../components/Product'
-import axios from 'axios'
+import { listProducts } from '../actions/productActions'
+
+// import axios from 'axios'
+// not making API call here anymore
 
 // import products from '../products'
 // this was the temporary products import
@@ -18,8 +22,8 @@ import axios from 'axios'
 
 function HomeScreen() {
 
-    const [products, setProducts] = useState([])
-
+    // const [products, setProducts] = useState([])
+    // local state not needed anymore
     
     //function is async and await returns the promise of the api url, data is destructured and then state is reset
     // empty array at end so useEffect updates when componend loads, not when state is changed
@@ -30,36 +34,65 @@ function HomeScreen() {
     //             console.log(res.data);  
     //     fetchProducts()
     // }, [])
+    // below code is better
 
+
+    // useEffect(() => {
+    //     console.log('useEffect triggered')
+    //     async function fetchProducts() {
+    //         const { data } = await axios.get('/api/products/')
+    //         setProducts(data)
+    //     }
+    //     fetchProducts()
+    // }, [])
+    // getting rid of API call here bc it is being made from productActions now
+
+
+    const dispatch = useDispatch()
+
+    const productList = useSelector(state => state.productList)
+
+    const {error, loading, products} = productList
 
     useEffect(() => {
-        console.log('useEffect triggered')
-        async function fetchProducts() {
-            const { data } = await axios.get('/api/products/')
-            setProducts(data)
-        }
-        fetchProducts()
+        dispatch(listProducts())
     }, [])
 
+    // const products = []
+    // solved no products error
 
     return (
         <div>
             <h1>Latest Products</h1>
 
+            {loading ? <h2>Loading...</h2>
+                : error ? <h3>{error}</h3>
+                    :
+                    <Row>
+                        {products.map(product => (
+                            <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                                <Product product={product} />
+                             </Col>
+                        ))}
+                    </Row>
+            }
+            {/* if loading render <h2>Loading...</h2>, if there is an error render the error, or if not render the Row  */}
+
 
             {/* iterate over products and put each one in a column */}
             {/* the parameters in Col is bootstrap for how many columns on what size screen */}
             {/* the key={product._id} gets rid of a react error that warns against not having unique keys when iterating over something, this is solved by using the unique attribute _id */}
-            <Row>
+            {/* <Row>
                 {products.map(product => (
                     <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-                        {/* <h3>{product.name}</h3> */}
+                        // <h3>{product.name}</h3>
 
-                        {/* Product components being passed product as a prop */}
+                        // Product components being passed product as a prop
                         <Product product={product} />
                     </Col>
                 ))}
-            </Row>
+            </Row> */}
+            {/* relocated this above */}
         </div>
     )
 }
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,10 +1,13 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
+import { productListReducer } from './reducers/productReducers'
 
 // https://redux.js.org/api/api-reference
 
-const reducer = combineReducers({})
+const reducer = combineReducers({
+    productList: productListReducer,
+})
 
 const initialState = {}
 
