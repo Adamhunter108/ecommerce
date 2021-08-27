7a5bb4d614aaeb8fc736f60f1f85f89cf0562a32
Adamhunter108 <61752774+Adamhunter108@users.noreply.github.com>
2021-08-27 11:16:02 -0400

    Created productListReducer

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -1,6 +1,18 @@
 
 // reducers are functions that takes in the current state and an action type what to do with the state and will update the store
 
-const produtListReducers (state ={ products: [] }, action) => {
-    case 'PRODUCT_LIST_REQUEST':
+export const productListReducer = (state ={ products: [] }, action) => {
+    switch (action.type) {
+        case 'PRODUCT_LIST_REQUEST':
+            return { loading: true, products: [] }
+
+        case 'PRODUCT_LIST_SUCCESS':
+            return { loading: false, products: action.payload }
+
+        case 'PRODUCT_LIST_FAIL':
+            return { loading: false, error: action.payload }
+
+        default:
+            return state
+    }
 }
