aa4b207c9bd56506741ca678a169f7172e045130
Adamhunter108 <61752774+Adamhunter108@users.noreply.github.com>
2021-08-26 23:34:25 -0400

    Created ProductSerializer and fixed static file errors

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -130,6 +130,14 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+MEDIA_URL = '/images/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+
+MEDIA_ROOT = 'static/images'
+
 
 # CORS_ALLOW_ALL_ORIGINS = True
 
--- backend/base/views.py
+++ backend/base/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from .products import products
+from .models import Product
+from .serializers import ProductSerializer
+# from .products import products
 
 
 # https://www.django-rest-framework.org/api-guide/views/#function-based-views
@@ -36,17 +38,21 @@ def getProducts(request):
     # return JsonResponse(products, safe=False)
     # above was before django rest framework was being used
 
-    return Response(products)
+    products = Product.objects.all()
+    serializer = ProductSerializer(products, many=True)
+    return Response(serializer.data)
 
 
 @api_view(['GET'])
 def getProduct(request, pk):
-    product = None
-    # loop through products array to find product with primary key (_id) that matches    
-    for i in products:
-        if i['_id'] == pk:
-            product = i
-            break
+    # product = None
+    # # loop through products array to find product with primary key (_id) that matches    
+    # for i in products:
+    #     if i['_id'] == pk:
+    #         product = i
+    #         break
 
-    return Response(product)
+    product = Product.objects.get(_id=pk)
+    serializer = ProductSerializer(product, many=False) 
+    return Response(serializer.data)
 
