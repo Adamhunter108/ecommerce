ca6a1127729e21fde6b61bd88b2d842f427eccfe
Adamhunter108 <61752774+Adamhunter108@users.noreply.github.com>
2021-08-27 10:16:08 -0400

    Started Redux store

---
+++ frontend/src/store.js
@@ -0,0 +1,15 @@
+import { createStore, combineReducers, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
+import { composeWithDevTools } from 'redux-devtool-extention'
+
+// https://redux.js.org/api/api-reference
+
+const reducer = combineReducers({})
+
+const initialState = {}
+
+const middleware = [thunk]
+
+const store = createStore(reducer, initialState, composeWithDevTools(applyMiddleware(...middleware)))
+
+export default store
