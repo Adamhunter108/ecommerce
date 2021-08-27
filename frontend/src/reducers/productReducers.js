483127ed6cefa8b6881b511ea660555908870183
Adamhunter108 <61752774+Adamhunter108@users.noreply.github.com>
2021-08-27 10:59:20 -0400

    Fixed redux-devtools spelling mistake causing errors

---
+++ frontend/src/reducers/productReducers.js
@@ -0,0 +1,6 @@
+
+// reducers are functions that takes in the current state and an action type what to do with the state and will update the store
+
+const produtListReducers (state ={ products: [] }, action) => {
+    case 'PRODUCT_LIST_REQUEST':
+}
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,6 +1,6 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
-import { composeWithDevTools } from 'redux-devtool-extention'
+import { composeWithDevTools } from 'redux-devtools-extension'
 
 // https://redux.js.org/api/api-reference
 
