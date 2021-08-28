356aedbb56e117990ad695ff4e4ea58bd9f833d9
Adamhunter108 <61752774+Adamhunter108@users.noreply.github.com>
2021-08-28 13:15:56 -0400

    Added products and productConstants

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,12 +1,13 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
-import { productListReducer } from './reducers/productReducers'
+import { productListReducer, productDetailsReducer } from './reducers/productReducers'
 
 // https://redux.js.org/api/api-reference
 
 const reducer = combineReducers({
     productList: productListReducer,
+    productDetails: productDetailsReducer,
 })
 
 const initialState = {}
